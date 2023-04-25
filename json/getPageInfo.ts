d9165bc89dcc26cbc7585f4b99e356cfc739148d
codecatalyst <codecatalyst@gmail.com>
2023-04-25 10:53:20 -0400

    fix deploy edge issue

---
+++ json/getPageInfo.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PageInfo } from '@/typings'
+// import pageInfoJson from '@/json/pageInfo.json'
+
+type Data = {
+  pageInfo: PageInfo
+}
+
+export const config = {
+	runtime: 'edge',
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const data: Response = await fetch(`${process.env.NEXT_DB_BASE_URL}/${process.env.NEXT_DB_INFO}/pageInfo`)
+  const pageInfo: PageInfo = await data.json()
+
+  res.status(200).json({ pageInfo })
+}
---
+++ json/getProjects.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Project } from '@/typings'
+// import Projects from '@/json/projects.json'
+
+type Data = {
+  projects: Project[]
+}
+
+export const config = {
+	runtime: 'edge',
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const data: Response = await fetch(`${process.env.NEXT_DB_BASE_URL}/${process.env.NEXT_DB_INFO}/projects`)
+  const projects: Project[] = await data.json() 
+  res.status(200).json({ projects })
+}
