9488a6c5c581bd554d8c34fd78d429693307b271
codecatalyst <codecatalyst@gmail.com>
2023-04-25 13:36:48 -0400

    fix deploy edge issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from 'framer-motion';
 import { Social } from '@/typings'
-"use client"
+import { useState, useEffect } from 'react';
+
 type Props = {
   socials: Social[]
 };
 
 function Header({ socials }: Props) {
+  const [hasMounted, setHasMounted] = useState(false);
+    // Hooks
+    useEffect(() => {
+      setHasMounted(true);
+  }, [])
+
+  // Render
+  if (!hasMounted) return null;
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div 
