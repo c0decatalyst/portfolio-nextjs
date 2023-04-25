8a3eab4766845ba22df0b12ccbf6111e05a07660
codecatalyst <codecatalyst@gmail.com>
2023-04-24 23:22:21 -0400

    fix deploy edge issue

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PageInfo } from '@/typings'
-// import pageInfoJson from '@/json/pageInfo.json'
+import pageInfoJson from '@/json/pageInfo.json'
 
 type Data = {
-  pageInfo: string
+  pageInfo: PageInfo
 }
 
 // export const config = {
@@ -15,6 +15,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // const pageInfo: PageInfo = pageInfoJson 
-  res.status(200).json({ pageInfo:"kkj" })
+  const pageInfo: PageInfo = pageInfoJson 
+  res.status(200).json({ pageInfo })
 }
