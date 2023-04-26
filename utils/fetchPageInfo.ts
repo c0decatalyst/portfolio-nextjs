61eb1b86594a5322cca26f7ad7f702b7052957d6
codecatalyst <codecatalyst@gmail.com>
2023-04-25 23:26:44 -0400

    fix deploy edge issue

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,6 +3,7 @@ import { PageInfo } from "../typings"
 export const fetchPageInfo = async() => {
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+        // `${process.env.NEXT_DB_BASE_URL}/${process.env.NEXT_DB_INFO}/socials`
     );
 
     const data = await res.json();
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -3,6 +3,7 @@ import { Project } from "../typings"
 export const fetchProjects = async() => {
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
+        // `${process.env.NEXT_DB_BASE_URL}/${process.env.NEXT_DB_INFO}/projects`
     );
 
     const data = await res.json();
