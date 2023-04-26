450f0c88971c92225dce47a2e2fd9b5c4fd252c2
codecatalyst <codecatalyst@gmail.com>
2023-04-25 23:16:50 -0400

    fix deploy edge issue

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,12 +1,15 @@
 import { Social } from "../typings"
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const fetchSocials = async() => {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`
+        // `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`
+        `${process.env.NEXT_DB_BASE_URL}/${process.env.NEXT_DB_INFO}/socials`
     );
 
     const data = await res.json();
     const socials: Social[] = data.socials;
 
+    // return res.status(200).json({ socials })
     return socials;
 }
