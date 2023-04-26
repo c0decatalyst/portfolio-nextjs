e40ec60c85ee8630b869743c903166afa8b58d55
codecatalyst <codecatalyst@gmail.com>
2023-04-25 23:54:16 -0400

    fix deploy edge issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,9 +93,9 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       projects,
       socials
     },
-    // revalidate: 10, // regen the page when incoming request at most every xx secs
+    revalidate: 10, // regen the page when incoming request at most every xx secs
   }
 }
-export const config = {
-	runtime: 'experimental-edge',
-}
+// export const config = {
+// 	runtime: 'experimental-edge',
+// }
